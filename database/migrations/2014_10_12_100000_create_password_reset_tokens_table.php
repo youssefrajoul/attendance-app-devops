4898e6cf3543e7f8af17e99a32dbf1cc402b7720
Youssef Rajoul <59939@etu.he2b.be>
2023-12-01 22:34:51 +0100

    Edit 2014_10_12_100000 migration

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            if (!Schema::hasTable('password_reset_tokens')) {
+                $table->string('email');
+                $table->string('token');
+                $table->timestamps();
+            }
         });
     }
 
